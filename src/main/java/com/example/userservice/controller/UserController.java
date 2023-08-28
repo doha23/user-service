95b70a520be13bda1600a1966719b22546268284
yunjisu <doha23@naonsoft.com>
2023-08-28 17:47:26 +0900

    [섹션4]Users Microservice - Users Microservice - JPA ②
    - Status -> 201 Created 성공 메세지 반환 및 반환 데이터 추가
    - 데이터베이스 테이블 자동생성

--- src/main/java/com/example/userservice/controller/UserController.java
+++ src/main/java/com/example/userservice/controller/UserController.java
@@ -2,6 +2,7 @@ package com.example.userservice.controller;
 
 import com.example.userservice.VO.Greeting;
 import com.example.userservice.VO.RequestUser;
+import com.example.userservice.VO.ResponseUser;
 import com.example.userservice.dto.UserDto;
 import com.example.userservice.service.UserService;
 import com.netflix.discovery.converters.Auto;
@@ -9,6 +10,8 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -38,13 +41,15 @@ public class UserController {
     }
 
     @PostMapping("/users")
-    public String createUser(@RequestBody RequestUser user) {
+    public ResponseEntity createUser(@RequestBody RequestUser user) {
         ModelMapper mapper = new ModelMapper();
         mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 
         UserDto userDto = mapper.map(user, UserDto.class);
         userService.createsUser(userDto);
 
-        return "Create user method is called";
+        ResponseUser responserUser = mapper.map(userDto, ResponseUser.class);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(responserUser);
     }
 }
