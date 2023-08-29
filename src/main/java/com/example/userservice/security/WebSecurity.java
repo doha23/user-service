eeb38044c15b7f564020d4d3cb902cb9ddd667ee
yunjisu <doha23@naonsoft.com>
2023-08-29 15:40:40 +0900

    [섹션4] Users Microservice - Spring Security 연동
    - Spring Security 연동 및 Password 암호화하여 데이터 베이스에 저장

---
+++ src/main/java/com/example/userservice/security/WebSecurity.java
@@ -0,0 +1,34 @@
+package com.example.userservice.security;
+
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurity {
+
+    private static final String[] WHITE_LIST = {
+            "/users/**",
+            "/**"
+    };
+
+    @Bean
+    protected SecurityFilterChain config(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
+        http.authorizeRequests(authorize -> authorize
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                .requestMatchers(new AntPathRequestMatcher("/**")).permitAll()
+                .anyRequest().authenticated()
+        );
+        return http.build();
+    }
+}
--- src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -6,6 +6,7 @@ import com.example.userservice.jpa.UserRepository;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -14,6 +15,13 @@ import java.util.UUID;
 public class UserServiceImpl implements UserService {
     @Autowired
     UserRepository userRepository;
+    BCryptPasswordEncoder passwordEncoder;
+    @Autowired // 사용자에 의해서 만들어진 것이 아니라, spring context가 기동이 되면서 자동으로 등록할 수 있는 Bean을 메모리에 객체를 생성해줌
+    public UserServiceImpl(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Override
     public UserDto createsUser(UserDto userDto) {
         userDto.setUserId(UUID.randomUUID().toString());
@@ -21,7 +29,7 @@ public class UserServiceImpl implements UserService {
         ModelMapper mapper = new ModelMapper();
         mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
         UserEntity userEntity = mapper.map(userDto, UserEntity.class);
-        userEntity.setEncryptedPwd("encrypted_password");
+        userEntity.setEncryptedPwd(passwordEncoder.encode(userDto.getPwd())); // 암호화 하여 Password 저장
 
         userRepository.save(userEntity);
 
