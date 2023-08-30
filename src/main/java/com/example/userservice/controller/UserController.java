18e020701c282e30ecdb4718b85d09365f3d4db3
yunjisu <doha23@naonsoft.com>
2023-08-30 11:24:12 +0900

    [섹션6] Users Microservice ➁ - Users Microservice - Routes 정보 변경
    - Path 제거

--- src/main/java/com/example/userservice/controller/UserController.java
+++ src/main/java/com/example/userservice/controller/UserController.java
@@ -6,7 +6,6 @@ import com.example.userservice.VO.ResponseUser;
 import com.example.userservice.dto.UserDto;
 import com.example.userservice.jpa.UserEntity;
 import com.example.userservice.service.UserService;
-import com.netflix.discovery.converters.Auto;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,7 +18,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/user-service")
+@RequestMapping("/")
 public class UserController {
     private Environment env;
     private UserService userService;
