b23ea17fbad8f6307dff6dfc1c7e6f8ab288d6dd
yunjisu <doha23@naonsoft.com>
2023-08-30 16:48:15 +0900

    [섹션6] Users Microservice ➁ - Users Microservice - 로그인 처리 과정
    - loggingLevel 추가 및 login 요청 유저 email log 확인

--- src/main/java/com/example/userservice/security/AuthenticationFilter.java
+++ src/main/java/com/example/userservice/security/AuthenticationFilter.java
@@ -2,9 +2,11 @@ package com.example.userservice.security;
 
 import com.example.userservice.VO.RequestLogin;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.servlet.FilterChain;
@@ -14,6 +16,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
 
+@Slf4j
 public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
     @Override
@@ -38,5 +41,7 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
                                             Authentication authResult) throws IOException, ServletException {
         //super.successfulAuthentication(request, response, chain, authResult); //RestAPI Server이기 때문에 로그인 페이지를 응답할 필요가 없음
+
+        log.debug(((User)authResult.getPrincipal()).getUsername());
     }
 }
